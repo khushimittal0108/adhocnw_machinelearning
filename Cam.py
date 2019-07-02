91825f3d149a172c38617dfbd7aa68bfc5043e72
khushimittal0108 <khushimittal0858@gmail.com>
2019-07-02 22:28:38 +0530

    Update Cam.py

--- Cam.py
+++ Cam.py
@@ -6,12 +6,12 @@ import cv2
 cap=cv2.VideoCapture(0)
 
 while cap.isOpened():
-	status,frame=cap.read()
+    status,frame=cap.read()
+    cv2.imshow('live1',frame)
+    if cv2.waitKey(10) & 0xff == ord('q'):
+        break
+    #cv2.waitKey(0)
 
-	cv2.imshow('live',frame)
-#	if cv2.waitkey(10) & 0xff == ord('q'):
-#		break
-cv2.waitKey(0)
 #cv2.destroyWindow('live')
 cv2.destroyAllWindows()  #this will destroy all windows
 # to close camera
